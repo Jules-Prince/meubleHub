355cea861f86e8e35adbb927c48612abb962814a
jules-prince <jules.prince.fr@icloud.com>
2025-01-27 23:38:59 +0100

    🔧 Update env configuration

--- backend/object-service/main.go
+++ backend/object-service/main.go
@@ -8,13 +8,9 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		utils.Log.Error("Warning: No .env file found")
-	}
 	
 	port := os.Getenv("PORT")
 	if port == "" {
