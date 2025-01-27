264d6364dfe98b80f29d41f63ede4291499bd9e6
jules-prince <jules.prince.fr@icloud.com>
2025-01-27 22:38:51 +0100

    🔊 #9 add log for room service

--- backend/room-service/main.go
+++ backend/room-service/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"hexagone/room-service/database"
 	"hexagone/room-service/services"
+	"hexagone/room-service/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	utils.InitLogger()
+	utils.Log.Info("Starting Object Service")
+
 	// Connect to the database
 	database.ConnectDatabase()
+	utils.Log.Info("Connected to SQLite")
 
 	r := gin.Default()
 
@@ -17,6 +22,7 @@ func main() {
 	r.POST("/rooms", services.CreateRoom)
 	r.GET("/rooms", services.ListRooms) 
 
+	utils.Log.Info("Starting HTTP server on port 8080")
 	// Start the service
 	r.Run(":8080") 
 }
