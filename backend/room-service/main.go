d322b71518ed7f24ddd51a9d2d20eabd29bbf18b
jules-prince <jules.prince.fr@icloud.com>
2025-01-27 23:17:29 +0100

    🔧 #8 add env to room service

--- backend/room-service/main.go
+++ backend/room-service/main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"hexagone/room-service/database"
 	"hexagone/room-service/services"
 	"hexagone/room-service/utils"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		utils.Log.Error("Warning: No .env file found")
+	}
+
+	// Get configuration from environment variables
+	port := os.Getenv("PORT")
+	if port == "" {
+		utils.Log.Error("PORT is not set in the environment variables")
+	}
+
+	dbPath := os.Getenv("DB_PATH")
+	if dbPath == "" {
+		utils.Log.Error("DB_PATH is not set in the environment variables")
+	}
+	
 	utils.InitLogger()
 	utils.Log.Info("Starting Object Service")
 
 	// Connect to the database
-	database.ConnectDatabase()
+	database.ConnectDatabase(dbPath)
 	utils.Log.Info("Connected to SQLite")
 
 	r := gin.Default()
@@ -22,7 +41,10 @@ func main() {
 	r.POST("/rooms", services.CreateRoom)
 	r.GET("/rooms", services.ListRooms) 
 
-	utils.Log.Info("Starting HTTP server on port 8080")
-	// Start the service
-	r.Run(":8080") 
+	utils.Log.Infof("Starting HTTP server on port %s", port)
+
+	// Start the server
+	if err := r.Run(fmt.Sprintf(":%s", port)); err != nil {
+		utils.Log.Fatalf("Failed to start server: %v", err)
+	}
 }
