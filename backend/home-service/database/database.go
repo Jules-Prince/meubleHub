d2899ef32a12ae388f7b7dd671e7b848f90349fd
jules-prince <jules.prince.fr@icloud.com>
2025-01-27 22:47:08 +0100

    🐛 Wrong file oups

--- backend/home-service/database/database.go
+++ backend/home-service/database/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("data/room.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("data/home.db"), &gorm.Config{})
 	if err != nil {
 		utils.Log.WithField("error", err.Error()).Error("Failed to connect to database")
 	}
